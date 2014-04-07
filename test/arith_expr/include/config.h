1d5e234d2952e8a7c43ea53f996506f0f52001cd
Alessandro Barenghi <alessandro.barenghi@gmail.com>
2014-04-07 08:37:34 +0200

    removed excessively large benchmarks

--- test/arith_expr/include/config.h
+++ test/arith_expr/include/config.h
@@ -1,23 +1,13 @@
 #ifndef CONFIG_H_
 #define CONFIG_H_
-
-/* define how the remaining reduction after the
- first parallel pass are to be recombined:
- by a single thread -> SINGLE_RECOMBINATION
- by half of the current working threads -> LOG_RECOMBINATION*/
+/* chunk recombination strategy */
 #define SINGLE_RECOMBINATION
-
-
-/* Handle identification stack parameters. */
-#define LIST_ALLOC_SIZE			1024
-
+/* Number of preallocated parsing stack symbols */
+#define LIST_ALLOC_SIZE 1024
 /* Average rhs length. */
-#define RHS_LENGTH				2.6f
-
+#define RHS_LENGTH 2.0f
 /* Average token size. */
-#define TOKEN_SIZE				1.1f
-
+#define TOKEN_SIZE 5.0f
 /* Length of a line of cache. */
-#define CACHE_LINE				64
-
+#define CACHE_LINE_SIZE 64
 #endif
